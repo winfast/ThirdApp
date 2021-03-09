2e15a65420a402f9eb07e7968505d697f2f69247
Qincc <418891087@qq.com>
2021-03-09 20:44:47 +0800

    更新代码

--- ThirdApp/Class/Controller/成员列表/ViewControllers/TSUserListViewController.m
+++ ThirdApp/Class/Controller/成员列表/ViewControllers/TSUserListViewController.m
@@ -7,10 +7,9 @@
 
 #import "TSUserListViewController.h"
 
-@interface TSUserListViewController ()
+@interface TSUserListViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView *tableView;
-//@property (nonatomic, copy) NSArray *<#object#>;
 
 @end
 
@@ -19,16 +18,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(0);
+    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)navigationViewLayout {
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"添加用户" style:UIBarButtonItemStylePlain target:self action:@selector(clickLeftBtn:)];
 }
-*/
+
+- (void)clickRightBtn
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCell.alloc.init;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [UITableView.alloc initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.rowHeight = UITableViewAutomaticDimension;
+        _tableView.estimatedRowHeight = 56;
+        _tableView.separatorColor = UIColor.lightGrayColor;
+    }
+    return _tableView;
+}
 
 @end
